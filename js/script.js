9647c20a3e27212472e697f0ae34e431b06fd2c6
alessandroschiano98 <schianoski98@gmail.com>
2025-11-03 13:06:34 +0100

    json(countries) + js commands

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// ! CARDS //
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const container = document.getElementById("cardList");
+
+  try {
+    const response = await fetch("../data/travels.json");
+    const travels = await response.json();
+
+    travels.forEach((t) => {
+      const card = document.createElement("a");
+      card.classList.add("card-item");
+      card.href = "#";
+      card.innerHTML = `
+        <img src="${t.img}" alt="Card Image">
+        <span class="data">${t.date}</span>
+        <h3>${t.city}</h3>
+        <p class="text-secondary fw-bold">${t.description}</p>
+        <div class="arrow">
+          <i class="fas fa-arrow-right card-icon"></i>
+        </div>
+      `;
+      container.appendChild(card);
+    });
+  } catch (error) {
+    console.error("Errore nel caricamento del JSON:", error);
+  }
+});
